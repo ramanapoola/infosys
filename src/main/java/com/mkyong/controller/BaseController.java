3c3810e73015471535030fc3c065cd2a7c2159b9
Your Name <ramana.poola0055@gmail.com>
2018-10-28 15:36:18 +0530

    new content added

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -17,7 +17,7 @@ public class BaseController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 
-		model.addAttribute("Deepikamessage", "Welcome");
+		model.addAttribute("Ramanamessage", "Welcome");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
 
